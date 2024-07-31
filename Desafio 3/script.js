ceea677849368febfda341b4c9efe9f0f0c33d18
Vanderson <vanderson.santos94@hotmail.com>
2024-07-31 00:37:26 -0300

    incluindo interatividade na pagina

---
+++ Desafio 3/script.js
@@ -0,0 +1,4 @@
+document.getElementById('searchIcon').addEventListener('click', function () {
+    const searchBox = document.getElementById('searchBox');
+    searchBox.style.display = searchBox.style.display === 'block' ? 'none' : 'block';
+});
